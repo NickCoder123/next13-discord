7040dd141e81beb2b0963040f2a1efe8a07686cd
Nikunj Mehra <nikunjmehra935@gmail.com>
2024-02-11 13:22:53 +0530

    Environment setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <p className="text-3xl text-rose-600 font-bold">Hello Discord Clone</p>
+    <div className="flex flex-col">
+      <p className="text-3xl text-rose-600 font-bold">Hello Discord Clone</p>
+      <Button>Click me!</Button>
+    </div>
   );
 }
